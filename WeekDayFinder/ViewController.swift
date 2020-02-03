e5705ba662cd7266ff34db167e5278d298af0155
Natallia <tarasevich.natallia@gmail.com>
2020-02-03 16:19:38 +0300

    Repozitory inicialization
    Repozitory inicialization

---
+++ WeekDayFinder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeekDayFinder
+//
+//  Created by Natallia Tarasevich on 1/30/20.
+//  Copyright © 2020 Natallia Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay() {
+    
+    
+    
+    }
+
+
+}
+
